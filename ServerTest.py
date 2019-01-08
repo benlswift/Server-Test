7a8af7c2d0cf67026381a6699e1b9cefe8cb6388
benlswift <ben.swift2016@my.ntu.ac.uk>
2019-01-08 21:36:34 +0000

    Added threading - run every 30secs

--- ServerTest.py
+++ ServerTest.py
@@ -1,15 +1,20 @@
 import os
 import tkinter as tk
+import urllib.request
+import threading
 
+def run_check():
+    threading.Timer(30.0, run_check).start()
+    fh = os.popen("ServerTest.bat")
+    output = fh.read()
+    print ("This is the output of ServerTest.bat:", output)
+    if "Request timed out" in output: 
+        print ("STATION OFF AIR - ALERT HEAD OF SYSTEMS")
+        master = tk.Tk()
+        OffAirMessage = "STATION OFF AIR - ALERT HEAD OF SYSTEMS"
+        msg = tk.Message(master, text = OffAirMessage)
+        msg.config(bg='red', font=('times', 50, 'bold'))
+        msg.pack()
+    fh.close()
 
-fh = os.popen("ServerTest.bat")
-output = fh.read()
-print ("This is the output of ServerTest.bat:", output)
-if "Request timed out" in output: 
-    print ("STATION OFF AIR - ALERT HEAD OF SYSTEMS")
-    master = tk.Tk()
-    whatever_you_do = "STATION OFF AIR - ALERT HEAD OF SYSTEMS"
-    msg = tk.Message(master, text = whatever_you_do)
-    msg.config(bg='red', font=('times', 50, 'bold'))
-    msg.pack()
-fh.close()
+run_check()
